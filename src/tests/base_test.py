9eea78bfb0b4bddb0b371efcd7f2fb2b6af8a44f
Adrien Gentil <agentil@redhat.com>
2023-01-24 17:03:51 +0100

    another typo

--- src/tests/base_test.py
+++ src/tests/base_test.py
@@ -310,7 +310,7 @@ class BaseTest:
     def day2_controller(
         self, day2_cluster_configuration: Day2ClusterConfig, day2_controller_configuration: BaseNodesConfig
     ) -> NodeController:
-        assert cluster_configuration.platform == consts.Platforms.BARE_METAL
+        assert day2_cluster_configuration.platform == consts.Platforms.BARE_METAL
         return TerraformController(day2_controller_configuration, entity_config=day2_cluster_configuration)
 
     @pytest.fixture
